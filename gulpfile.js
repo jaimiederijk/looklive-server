cbc58cc9f89b87a20cc1e27a162b4bacfc9ad692
jaimiederijk <j.r.derijk@quicknet.nl>
2016-05-16 23:39:17 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,64 @@
-var gulp = require('gulp'),
-    svgSprite = require('gulp-svg-sprite');
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
 
-var svgConfig = {
-    dest: '.',
-    shape: {
-        dimension: {
-            maxWidth: 15,
-            maxHeight: 15
-        },
-        spacing: {
-            padding: 1,
-        },
-    },
-    mode: {
-        css: {
-            dest: '.',
-            sprite: 'sprite.svg',
-            render: {
-                css: true
-            },
-            example: true,
-            prefix: '.icn-'
-        }
-    }
-};
+gulp.task('images', function() {
+  return gulp.src('public/images/*')  
+    .pipe(plumber({errorHandler: onError}))
 
-gulp.task('icons', function () {
-    gulp.src('./public//icons/svg/*.svg')
-        .pipe(svgSprite(svgConfig))
-        .pipe(gulp.dest('./public/icons/sprite/'));
+    // Image optimization
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))//
+    
+    // Distribute to build
+    .pipe(gulp.dest('public/images'))
+
+
 });
+
+
+gulp.task('styles', function() { 
+  return gulp.src('public/styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('public/styles'));    // Catch errors
+
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('public/js/**/*.js')
+    // Catch errors
+    .pipe(plumber({errorHandler: onError}))
+
+    // Concatinate in one file
+    .pipe(concat('main.js'))
+
+    // Add a .min version
+    .pipe(rename({ suffix: '.min' }))
+
+    //Minify with jsUglify
+    .pipe(uglify())
+
+    // Distribute to build
+    .pipe(gulp.dest('public/js'))
+
+
+});
+
+gulp.task('setWatch', function() {
+  global.isWatching = true;
+});
+
+// Watch
+gulp.task('watch', ['setWatch'], function() {
+
+  // Watch .scss files
+  gulp.watch('public/styles/*.css', ['styles']);
+  gulp.watch('public/js/*.js', ['scripts']);
+  gulp.watch('public/images/**/*', ['images']);
+});
+
+gulp.task('default', ['styles', 'scripts','images']);
